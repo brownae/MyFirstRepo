e1946a862fa6eddca26eef190ba5eb2643022e55
brownae <aebrown9@gmail.com>
2016-11-03 17:00:28 -0700

    Test1
    I put all the site files in MyFirstRepo. on my computer.

---
+++ levf/index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Lift Every Voice Foundation</title>
+<meta charset="UTF-8" />
+<meta name="robots" content="noindex, nofollow" />
+<meta name="viewport" content="initial-scale=1"/>
+<link href="style.css" rel="stylesheet"/>
+<!--for img slider start-->
+<link href="css/ideal-image-slider.css" rel="stylesheet"/>
+<link rel="stylesheet" href="css/themes/default/default.css">
+<!--for img slider end-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="js/script.js"></script>
+
+<!-- Remy Sharp Shim --> 
+<!--[if lt IE 9]>
+<script src="js/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+</head>
+<body> 
+<header>
+    
+    <h1>
+      <a href="index.html">
+        <span>Lift every Voice foundation</span> <!-- for SEO -->
+        <img src="images/site-logo.jpg" id="logo-main" alt="Lift Every Voice Foundation Logo" >
+      </a>
+    </h1>    
+
+<img src="images/donate-btn.jpg" id="dnt-btn" alt="Donate button" >
+<div id="search-bx">   
+        <input type="text" name="search" value="">
+        <input type="button" value="search">
+</div><!-- wordpress search box -->
+
+<!-- MAIN NAV - START -->
+<nav class="main-nav">       
+    <ul>
+       <li class="" id=""><a href=""><span>About</span></a>
+          <ul>
+             <li class=""><a href=''><span>Our Mission</span></a></li>
+             <li class=""><a href=''><span>Our Story</span></a></li>
+             <li class=""><a href=''><span>Our Partners</span></a></li>
+             <li class="last"><a href=''><span>Contact us</span></a></li>
+          </ul>
+       </li>
+       <li class="" id=""><a href=""><span>Projects</span></a>
+          <ul>
+             <li class=""><a href=''><span>Project 1</span></a></li>
+             <li class=""><a href=''><span>Project 2</span></a></li>
+             <li class=""><a href=''><span>Project 3</span></a></li>
+             <li class="last"><a href=''><span>Project 4</span></a></li>
+          </ul>
+       </li>
+       <li id=""><a href=""><span>Get Involved</span></a>
+          <ul>
+             <li class=""><a href=''><span>I am a non-profit</span></a></li>
+             <li class=""><a href=''><span>I am a school</span></a></li>
+             <li class=""><a href=''><span>I am a parent/student</span></a></li>
+             <li class="last"><a href=''><span>I would like to donate</span></a></li>
+          </ul>
+       </li>
+       <li id=""><a href=""><span>Blog</span></a></li>
+       <li id=""><a href=""><span>Contact</span></a></li>
+    </ul>
+</nav>    
+<!-- MAIN NAV - END -->
+</header>
+<main id="home-page"> 
+    
+<!-- Slider and add images START -->
+    <div id="slider">
+        <a href=''><img src="images/slider-img1.jpg" alt=""></a>
+        <a href=''><img data-src="images/slider-img2.jpg" src="" alt=""></a>
+        <a href=''><img data-src="images/slider-img3.jpg" src="" alt=""></a>
+        <a href=''><img data-src="images/slider-img4.jpg" src="" alt=""></a>
+    </div>
+    <!-- Include slider JS file -->
+    <script src="js/ideal-image-slider.js"></script>
+    <!-- Create your slider -->
+    <script>
+    var slider = new IdealImageSlider.Slider({
+    selector: '#slider',
+    interval: 6000,
+    transitionDuration: 1000
+});
+slider.start();
+    </script>
+<!-- Slider and add images END -->
+    
+    <div class="seo">
+            
+        <h2>Lift Every Voice Foundation</h2>
+            
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed diam quis lacus sodales ultrices ut in neque. Nunc dapibus convallis pellentesque. Pellentesque lacinia enim egestas purus eleifend convallis. Aenean congue consequat lectus vitae aliquet. Etiam finibus, nisl ac dignissim rhoncus, odio nisl vestibulum arcu, vitae feugiat arcu est ut augue. In nibh metus, laoreet a enim non, eleifend vehicula velit. Nam a consectetur nibh.</p>
+    
+    </div>
+    
+    <div id="cta-row">
+    <div class="cta" id="cta1">
+        <img class="cta-img" src="images/cta-pic1.jpg" alt="">
+        <div class="cta-content">
+        <h3>Headline 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+    </div>
+    
+    <div class="cta" id="cta2">
+        <img class="cta-img" src="images/cta-pic2.jpg" alt="">
+        <div class="cta-content">
+        <h3>Headline 2</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+    </div>
+    
+    <div class="cta" id="cta3">
+        <img class="cta-img" src="images/cta-pic3.jpg" alt="">
+        <div class="cta-content">
+        <h3>Headline 3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+    </div>
+    </div>
+    
+</main>
+<footer>
+    <ul>
+       <li class="" id=""><a href=""><span>About</span></a>
+          <ul>
+             <li class=""><a href=''><span>Our Mission</span></a></li>
+             <li class=""><a href=''><span>Our Story</span></a></li>
+             <li class=""><a href=''><span>Our Partners</span></a></li>
+             <li class="last"><a href=''><span>Contact us</span></a></li>
+          </ul>
+       </li>
+       <li class="" id=""><a href=""><span>Projects</span></a>
+          <ul>
+             <li class=""><a href=''><span>Project 1</span></a></li>
+             <li class=""><a href=''><span>Project 2</span></a></li>
+             <li class=""><a href=''><span>Project 3</span></a></li>
+             <li class="last"><a href=''><span>Project 4</span></a></li>
+          </ul>
+       </li>
+       <li id=""><a href=""><span>Get Involved</span></a>
+          <ul>
+             <li class=""><a href=''><span>I am a non-profit</span></a></li>
+             <li class=""><a href=''><span>I am a school</span></a></li>
+             <li class=""><a href=''><span>I am a parent/student</span></a></li>
+             <li class="last"><a href=''><span>I would like to donate</span></a></li>
+          </ul>
+       </li>
+       <li id=""><a href=""><span>Blog</span></a></li>
+       <li id=""><a href=""><span>Contact</span></a></li>
+       <li id=""><a href=""><span><img src="images/twitter-logo.jpg" alt="Twitter Logo"></span></a></li>
+    </ul>
+<!-- begin validation buttons -->
+    <p class="validation">
+    <a href="http://validator.w3.org/check?uri=referer">
+      <img src="http://www.w3.org/html/logo/badge/html5-badge-h-solo.png" 
+      width="31" height="32" alt="valid HTML5" />
+     </a>
+     <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">
+      <img src="http://jigsaw.w3.org/css-validator/images/vcss" 
+       alt="Valid CSS3" height="31" width="88" />
+    </a>
+    </p>
+<!-- end validation buttons -->
+    <div class="copyright">
+        <small>&copy; 2016, All Rights Reserved ~ Authored by The Seattle Central Raging Stallions </small>
+    </div>
+</footer>
+</body>
+</html>
