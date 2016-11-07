1622fcb985c2762936eead8d4967afb6d8393509
brownae <aebrown9@gmail.com>
2016-11-07 06:28:32 -0800

    Sliced it up!! Now has header.php and footer.php

---
+++ footer.php
@@ -0,0 +1,101 @@
+<!--Footer starts here-->
+<footer>
+    <div class="footer-gray">
+        <div class="footer-custom">
+            <div class="footer-lists">
+                <div class="footer-list-wrap">
+                    <h6 class="ftr-hdr">About</h6>
+                    <ul class="ftr-links-sub">
+                    </ul>
+                    <h6 class="ftr-hdr">Projects</h6>
+                    <ul class="ftr-links-sub">
+                        <li><a href="http://www.art.fr" rel="nofollow">Seattle</a></li>
+                        <li><a href="http://www.art.co.uk" rel="nofollow">Global Initiative</a></li>
+                    </ul>
+                </div>
+                <!--/.footer-list-wrap-->
+                <div class="footer-list-wrap">
+                    <h6 class="ftr-hdr">Get Involved</h6>
+                    <!--<ul class="ftr-links-sub">
+                        <li><a href="/help/talktous.html" rel="nofollow">Contact Us</a></li>
+                        <li><a href="/help/placingorders.html" rel="nofollow">Ordering</a></li>
+                        <li><a href="/help/shipping.html" rel="nofollow">Shipping &amp; Delivery</a></li>
+                        <li><a href="/help/shippingreturns.html" rel="nofollow">Returns</a></li>
+                        <li><a href="/help/international-shipping.html" rel="nofollow">International Orders</a></li>
+                        <li><a href="/~/egift-cards/" rel="nofollow">Gift Cards</a></li>
+                        <li><a href="/help/faq.html" rel="nofollow">FAQs</a></li>
+                    </ul>-->
+                </div>
+                <div class="footer-list-wrap">
+                    <h6 class="ftr-hdr">Blog</h6>
+                   <!-- <ul class="ftr-links-sub">
+                        <li><a href="/asp/aboutus/default-asp/_/posters.htm" rel="nofollow">Our Company</a></li>
+                        <li><a href="http://corporate.art.com/careers" rel="nofollow">Careers</a></li>
+                        <li><a href="/asp/landing/artistrising" rel="nofollow">Artist Rising</a></li>
+                        <li><a href="/~/art-for-business" rel="nofollow">Business &amp; Trade Sales</a></li>
+                        <li><a href="http://affiliates.art.com/index.aspx" rel="nofollow">Affiliate Program</a></li>
+                        <li><a href="/catalog" rel="nofollow"><strong>Shop Our Catalog</strong></a></li>
+                        <li><a href="http://blog.art.com" rel="nofollow">ART.COM BLOG</a></li>
+                    </ul>-->
+                </div>
+                <!--/.footer-list-wrap-->
+                <div class="footer-list-wrap">
+                    <h6 class="ftr-hdr">Contact</h6>
+                    <!--<ul class="ftr-links-sub">
+                        <art:content rule="!loggedin">
+                            <li class="ftr-Login"><span class="link login-trigger">Access My Account</span></li>
+                            <li><span class="link" onclick="link('/asp/secure/your_account/track_orders-asp/_/posters.htm')">Track My Order</span></li>
+                        </art:content>
+                        <art:content rule="loggedin">
+                            <li class="ftr-Login"><span class="link ftr-access-my-account">Access My Account</span></li>
+                            <li><span class="link" onclick="window.location.href = getProfileKey() + '?pagetype=oh';">Track My Order</span></li>
+                        </art:content>
+                    </ul>-->
+                </div>
+                <!--/.footer-list-wrap-->
+            </div>
+            <!--/.footer-lists-->
+            <div class="footer-email">
+                <h6 class="ftr-hdr">Newsletter</h6>
+                <div id="ftr-email" class="ftr-email-form">
+                    <form id="ftrEmailForm" method="post" action="http://em.art.com/pub/rf">
+                        <div class="error">Please enter a valid email address</div>
+                        <input type="text" name="email_address_" id="ftrEmailInput" class="input" placeholder="Enter email address" />
+                        <!--
+                            -->
+                        <input type="submit" class="button" value="SUBMIT" />
+                        <input type="hidden" name="country_iso2" value="">
+                        <input type="hidden" name="language_iso2" value="en">
+                        <input type="hidden" name="site_domain" value="art.com">
+                        <input type="hidden" name="email_acq_source" value="01-000001">
+                        <input type="hidden" name="email_acq_date" value="" id="ftr-email-date">
+                        <input type="hidden" name="brand_id" value="ART">
+                        <input type="hidden" name="_ri_" value="X0Gzc2X%3DWQpglLjHJlYQGnp51yrMf2qXdC9tjU8pzgMtwfYzaVwjpnpgHlpgneHmgJoXX0Gzc2X%3DWQpglLjHJlYQGnyLSq2fzdkuzdzglHMsUhgeNzaSgkk">
+                    </form>
+                </div>
+                <!--/.ftr-email-form-->
+                <div class="ftr-email-privacy-policy"></div>
+            </div>
+            <!--/.footer-email-->
+            <div class="footer-social">
+                <h6 class="ftr-hdr">Follow Us</h6>
+                <ul>
+                    <li>
+                        <a href="https://www.twitter.com/artdotcom" title="Twitter" onclick="_gaq.push(['_trackSocial', 'Twitter', 'Follow', 'Footer', 'undefined', 'True']);">
+                            <img width="67" alt="Follow us on Twitter" src="http://cache1.artprintimages.com/images/jump_pages/rebrand/footer/twitter.png">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!--/.footer-social-->
+            <div class="footer-legal">
+                <p>&copy; All Rights Reserved. | <a href="" rel="nofollow">Privacy Policy</a> | <a href="" rel="nofollow">Terms of Use</a>
+                    | <a href="" rel="nofollow">Terms of Sale</a></p>
+            </div>
+        </div>
+        <!--/.footer-custom-->
+    </div>
+    <!--/.footer-gray-->
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Lift Every Voice Foundation</title>
+<meta charset="UTF-8" />
+<meta name="robots" content="noindex, nofollow" />
+<meta name="viewport" content="initial-scale=1"/>
+<link href="style.css" rel="stylesheet"/>
+<!--for img slider start-->
+<link href="css/ideal-image-slider.css" rel="stylesheet"/>
+<link rel="stylesheet" href="css/themes/default/default.css">
+<!-- Remy Sharp Shim -->
+    <script src="js/html5.js" type="text/javascript"></script>
+<!--[if lt IE 9]>
+<![endif]-->
+</head>
+<body>
+<!--Header starts here-->    
+<header>
+<h1>
+    <a href="index.php">
+        <span>Lift every Voice foundation</span> <!-- for SEO -->
+        <img src="images/site-logo.jpg" id="logo-main" alt="Lift Every Voice Foundation Logo" >
+    </a>
+</h1>
+<img src="images/donate-btn.jpg" id="dnt-btn" alt="Donate button" >
+<div id="search-bx">
+    <input type="text" name="search" value="">
+    <input type="button" value="search">
+</div><!-- wordpress search box -->
+
+    <nav>
+        <label for="drop" class="toggle">Menu</label>
+        <input type="checkbox" id="drop" />
+        <ul class="menu">
+            <li><a href="#">About</a></li>
+            <li>
+                <!-- First Tier Drop Down -->
+                <label for="drop-1" class="toggle">Projects</label>
+                <a href="#">Projects</a>
+                <input type="checkbox" id="drop-1"/>
+                <ul>
+                    <li><a href="#">info</a></li>
+                    <li><a href="#">info</a></li>
+                    <li><a href="#">info</a></li>
+                </ul>
+            </li>
+            <li>
+                <!-- First Tier Drop Down -->
+                <label for="drop-2" class="toggle">info +</label>
+                <a href="#">Get Involved</a>
+                <input type="checkbox" id="drop-2"/>
+                <ul>
+                    <li>
+                        <!-- Second Tier Drop Down -->
+                        <label for="drop-3" class="toggle">Tutorials +</label>
+                        <a href="#">Tutorials</a>
+                        <input type="checkbox" id="drop-3"/>
+                        <ul>
+                            <li><a href="#">Community</a></li>
+                            <li><a href="#">Public Speaking</a></li>
+                            <li><a href="#">Other</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Resources</a></li>
+                    <li><a href="#">Links</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+<!--Header Ends here--> 
