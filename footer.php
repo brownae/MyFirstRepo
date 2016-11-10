1158e5aa81176be720221b7b8d53088ffa6995dc
brownae <aebrown9@gmail.com>
2016-11-10 15:02:45 -0800

    Made index.php and updated css changes

--- footer.php
+++ footer.php
@@ -24,7 +24,6 @@
                         <li><a href="" rel="nofollow">I am a non&dash;profit</a></li>
                         <li><a href="" rel="nofollow">I am a School</a></li>
                         <li><a href="" rel="nofollow">I am a parent&#47;student</a></li>
-                        <li><a href="" rel="nofollow">I am a non-profit</a></li>
                     </ul>
                 </div>
                 <div class="footer-list-wrap">
@@ -98,5 +97,17 @@
     </div>
     <!--/.footer-gray-->
 </footer>
+<!-- begin validation buttons -->
+    <p class="validation">
+    <a href="http://validator.w3.org/check?uri=referer">
+      <img src="http://www.w3.org/html/logo/badge/html5-badge-h-solo.png" 
+      width="31" height="32" alt="valid HTML5" />
+     </a>
+     <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">
+      <img src="http://jigsaw.w3.org/css-validator/images/vcss" 
+       alt="Valid CSS3" height="31" width="88" />
+    </a>
+    </p>
+<!-- end validation buttons -->
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include "header.php" ?>    
+<div id='wrapper'>
+<main id="index">
+    
+    <h2>Gateway 2</h2>
+    
+      <div class="postBox">    
+            <img class="left" src="images/thumbnail.jpg" alt="thumbnail">
+          
+            <a href=""><h3>H3 Sample Heading</h3></a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in orci vitae urna bibendum commodo. Duis congue mauris diam,sed dapibus ex ultrices a. Fusce venenatis <i><a href="">more...</a></i></p>
+      </div>
+    
+     <div class="postBox">    
+            <img class="left" src="images/thumbnail.jpg" alt="thumbnail">
+          
+            <a href=""><h3>H3 Sample Heading</h3></a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in orci vitae urna bibendum commodo. Duis congue mauris diam,sed dapibus ex ultrices a. Fusce venenatis <i><a href="">more...</a></i></p>
+      </div>
+    
+    
+    
+  
+</main>
+
+<sidebar id="primary">
+    <h4>Recent Blog Post</h4>
+    <img src="images/twitter-logo.jpg" alt="test img">
+    <p><strong>Blog Post Title</strong></p>
+    <p>Author, Sept 20 2016</p>
+
+</sidebar>
+</div>
+<?php include "footer.php" ?>
